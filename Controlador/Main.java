006f85e7f8bb8eadcddaf80bff99f2680a10003f
SrDeiby <deibyruiz04@gmail.com>
2024-05-07 22:10:41 -0600

    Ya terminado

---
+++ Controlador/Main.java
@@ -0,0 +1,13 @@
+package Controlador;
+import Vista.Menu;
+import java.awt.*;
+public class Main {
+public static void main(String[] Ars){
+    Menu llamar = new Menu();
+    llamar.setBounds(0,0,670,450);
+    llamar.setVisible(true);
+    llamar.setLocationRelativeTo(null);
+    llamar.getContentPane().setBackground(Color.WHITE);//Color    
+
+    }//Fin del main
+}
